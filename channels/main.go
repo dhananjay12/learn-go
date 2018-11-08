fe52599bd3e2fec9d2b98859695c8ab3438ca82f
Dhananjay Singh <dhananjay1204@gmail.com>
2018-11-09 00:14:16 +0530

    Channels fix

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -19,17 +20,22 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	fmt.Println(<-c)
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Its up"
+	c <- link
 }
